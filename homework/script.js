71503fb444b017d43390d7a5a59d9b0e4ede5b37
danyloposhenov <dendicuz01@gmail.com>
2023-01-29 15:04:10 +0200

    added base

---
+++ homework/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Exercise 1
+
+// function amount() {
+//     let count = 0;
+//     return function inner(step){
+//         count = count + step;
+//         console.log(count);
+//     }
+// }
+
+// let sum = amount();
+// sum(3);
+// sum(5);
+// sum(228);
+
+
+
+
+// Exercise 2
+
